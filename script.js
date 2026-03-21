2a9f3b2e40abaece6cec63f71bd560ddb893f258
Fabianplakalliu <fabianplakalliu@gmail.com>
2026-03-21 18:00:24 +0200

    big changes

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(e){
-    e.preventDefault()
-    
-    const target = document.querySelector(this.getAttribute('href'))
-    
-    target.scrollIntoView({
-    behavior:'smooth'
-    })
-    })
-    })
+ const elements = document.querySelectorAll('.fade-in')
+
+window.addEventListener('scroll', () => {
+elements.forEach(el => {
+const top = el.getBoundingClientRect().top
+
+if(top < window.innerHeight - 100){
+el.classList.add('show')
+}
+})
+})
+
+
+document.querySelectorAll('a').forEach(link=>{
+link.addEventListener('click',function(e){
+if(this.getAttribute('href').startsWith('#')){
+e.preventDefault()
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+behavior:'smooth'
+})
+}
+})
+})
