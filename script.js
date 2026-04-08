543efa49946637fd490c6b54f0eb8a51f99c485a
Fabianplakalliu <fabianplakalliu@gmail.com>
2026-04-08 22:16:05 +0300

    improve auth page UI

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ const appearOnScroll = new IntersectionObserver(function(entries, observer) {
 faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeModal = document.querySelector(".close-modal");
+const images = document.querySelectorAll(".clickable-image");
+
+images.forEach((img) => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+  });
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
